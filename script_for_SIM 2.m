34f3272b2e927cdfc9e2380066b136ef5bc6c8aa
martinacorda05 <151824440+martinacorda05@users.noreply.github.com>
2025-12-09 15:35:56 +0100

    Add files via upload

---
+++ script_for_SIM 2.m
@@ -0,0 +1,186 @@
+
+% This code generates 3 brain sources with specific phase syncrhonization for
+% a certain time interval, which can be used as ground-truth to test
+% time-verying connectivity approaches 
+
+% Is is based on the work of Gordon et al. 
+
+% Gordon S M, Franaszczuk P J, Hairston W D, Vindiola M and McDowell K 2013 
+% Comparing parametric and nonparametric methods for detecting 
+% phase synchronization in EEG J Neurosci Methods 212 247–58Gordon S M, 
+% Franaszczuk P J, Hairston W D, Vindiola M and McDowell K 2013
+
+%%
+clc
+clear all
+close all
+
+%% Parameters
+
+fs = 250;  % Sampling frequency
+time=1; % length of the simulated signal in seconds
+t = 0:1/fs:time-1/fs; % time vector  
+num_trials = 100; % number of trials 
+frequencies = linspace(1, 50, 50); % vector of frequencies from 1Hz to 50Hz 
+num_sources = 3; % number of sources generated
+num_samples = length(t); %number of samples 
+SNR=10; % signal to noise ratio
+n_rep=50; % number of time to repeat the signal generation
+alpha = [8, 13];  % frequency range of the syncrhonization (arbitrarly defined)
+start_and_length = [0.3 0.2]; % vector of the time point representing the onset of the syncrhonization and the duration (arbitrarly defined)
+% if other syncrhonization intervals are defined, you should add a row
+brain_sources_50_noise={}; % cell initialization for the results 
+
+%%
+for rep=1:n_rep
+
+% variables initialization 
+brain_sources = zeros(num_sources, num_samples, num_trials);
+noise= zeros(num_sources, num_samples, num_trials);
+
+for trial = 1:num_trials
+    
+    s1 = zeros(1, num_samples); % source number 1 
+    s2 = zeros(1, num_samples); % source number 2 
+    s3 = zeros(1, num_samples); % source number 3
+    
+    for f = frequencies
+        amplitude=1/f; %1/f amplitude effect
+        phase1 = 2*pi*rand;
+        phase2 = 2*pi*rand;
+
+        s1 = s1 + amplitude * sin(2 * pi * f * t + phase1);
+        if f >= alpha(1) && f <= alpha(2) 
+            for period = 1:size(start_and_length, 1)
+                phase_sync=phase1+pi/4; % imposed synchronization (arbitrarly defined)
+                start_sync_idx = round(fs * start_and_length(period, 1)); % index where syncrhonization starts
+                end_sync_idx = round(fs * (start_and_length(period, 1) + start_and_length(period, 2))); % index where syncronization ends 
+                s2(start_sync_idx:end_sync_idx) = s2(start_sync_idx:end_sync_idx) + amplitude * sin(2 * pi * f * t(start_sync_idx:end_sync_idx) + phase_sync);
+            end
+
+            s2(1:start_sync_idx-1) = s2(1:start_sync_idx-1) + amplitude * sin(2 * pi * f * t(1:start_sync_idx-1) + phase2); %fase due random
+            s2(end_sync_idx+1:end) = s2(end_sync_idx+1:end) + amplitude * sin(2 * pi * f * t(end_sync_idx+1:end) + phase2);
+        
+        else
+            s2 = s2 + amplitude * sin(2 * pi * f * t + phase2); 
+        end
+    end
+    
+    % s3 is not syncrhonized 
+    for f = frequencies
+        amplitude=1/f; 
+        phase = 2*pi*rand;
+        s3 = s3 + amplitude * sin(2 * pi * f * t + phase);
+    end
+    
+    % add random noise 
+    wn=randn(1,size(s1,2)); 
+    noise1=(norm(s1)/ (norm(wn) * sqrt (SNR))) * wn; 
+
+    wn=randn(1,size(s2,2)); 
+    noise2=(norm(s2)/ (norm(wn) * sqrt (SNR))) * wn; 
+
+    wn=randn(1,size(s3,2)); 
+    noise3=(norm(s3)/ (norm(wn) * sqrt (SNR))) * wn;
+
+    noise(1,:,trial)=noise1; 
+    noise(2,:,trial)=noise2; 
+    noise(3,:,trial)=noise3; 
+
+
+    brain_sources(1, :, trial) = s1;
+    brain_sources(2, :, trial) = s2;
+    brain_sources(3, :, trial) = s3; 
+end
+
+%%
+brain_sources_new=brain_sources+noise; 
+% reshape of the brain sources matrix (number of trials, num sources,
+% number of samples) 
+
+Y=zeros(num_trials,num_sources,num_samples); 
+for i=1:num_trials 
+    Y(i,:,:)=brain_sources_new(:,:,i); 
+end 
+
+brain_sources_50_noise{rep}=Y; % values are saved in a cell
+end 
+
+%% save the results 
+% save('brain_sources_100','brain_sources'); 
+
+%% Plotting one of the repetition
+
+% signal with
+
+figure 
+plot(t,squeeze(brain_sources(1,:,1))); 
+hold on 
+plot(t,squeeze(brain_sources(2,:,1)));
+hold on 
+plot(t, squeeze(brain_sources(3,:,1))); 
+xlabel('time')
+ylabel('amplitude')
+title('simulated sources through sinusoidal model')
+
+%%
+
+figure 
+plot(t,noise(1,:,1)); 
+hold on 
+plot(t,noise(2,:,1));
+hold on 
+plot(t, noise(3,:,1)); 
+xlabel('time')
+ylabel('amplitude')
+title('noise')
+
+%%
+
+figure 
+plot(t,brain_sources_new(1,:,1)); 
+hold on 
+plot(t,brain_sources_new(2,:,1));
+hold on 
+plot(t, brain_sources_new(3,:,1)); 
+xlabel('time')
+ylabel('amplitude')
+title('noisy brain sources')
+
+%% Spettro 
+
+% Plot e spettro del segnale simulato 
+
+[p1, fx]=pwelch(brain_sources_new(1,:,2),[],[],[],fs);
+[p2, fx]=pwelch(brain_sources_new(2,:,2),[],[],[],fs);
+[p3, fx]=pwelch(brain_sources_new(3,:,2),[],[],[],fs);
+
+figure
+plot(fx,10*log(p1))
+hold on 
+plot(fx,10*log(p2))
+hold on 
+plot(fx,10*log(p3))
+xlim([0 50])
+xlabel('frequency')
+ylabel('power')
+title('Welch power spectral density')
+
+%%
+
+[p1, fx]=pwelch(brain_sources(1,:,2),[],[],[],fs);
+[p2, fx]=pwelch(brain_sources(2,:,2),[],[],[],fs);
+[p3, fx]=pwelch(brain_sources(3,:,2),[],[],[],fs);
+
+figure
+plot(fx,10*log(p1))
+hold on 
+plot(fx,10*log(p2))
+hold on 
+plot(fx,10*log(p3))
+xlim([0 50])
+xlabel('frequency')
+ylabel('power')
+title('Welch power spectral density')
+
+%%
---
+++ script_for_SIM_1.m
@@ -0,0 +1,211 @@
+
+clc
+clear all
+close all 
+
+%% Add path 
+%%
+% To run this code, the SEED-G toolbox is required 
+
+% Anzolin A, Toppi J, Petti M, Cincotti F and Astolfi L 2021 
+% Seed-g: Simulated eeg data generator for testing connectivity 
+% algorithms Sensors 21
+
+addpath(genpath('C:\Users\marti\OneDrive - Politecnico di Milano\Progetto Dottorato\SEED-G-toolbox-main'));
+ 
+%% Load of real sources from the toolbox for AR components 
+path='C:\Users\marti\OneDrive - Politecnico di Milano\Progetto Dottorato\SEED-G-toolbox-main\real data\';
+name='sLOR_cortical_sources.mat'; 
+load([path name]);
+data=EEG.samp; 
+fs=EEG.fSamp; 
+
+%% Parameters 
+Nodes=3; % number of nodes 
+p=5; % Order of the model 
+DataLength=250; % Length of the simulated data
+Trials =100; % Number of trials 
+SNR =10; % Signal to noise ratio 
+
+
+%% Model definition 
+% connection positions and values 
+Model=zeros(Nodes,Nodes,2); 
+
+Model(2,3,1)=0; 
+Model(2,1,1)=0; 
+Model(3,1,1)=0.9; 
+
+Model(2,3,2)=0; 
+Model(2,1,2)=0.9;
+Model(1,3,2)=0; 
+
+% Delay matrix --> number of samples lag 
+DelayMatrix=zeros(Nodes,Nodes); 
+
+DelayMatrix(2,3)=0;
+DelayMatrix(3,1)=3;
+DelayMatrix(2,1)=3;
+
+%%
+trans_null=0; % this parameter defines a step transition 
+
+%%
+%%% Time-Varying Implementation Parameters (see SEED-G TOOLBOX for more
+%%% details)
+
+CampIntPerc=[0.5 0.5]; 
+CampInt=round(CampIntPerc*DataLength);
+NumIntCost=length(CampInt);
+Del_Range = 1:p;
+
+%%
+%%%Input for MVGC-Toolbox function 'var_to_tsdata'
+
+mtrunc=     0;       %default
+decayfac=   100;     %default
+Singtr=     1;
+
+%%% Threshold for the signal amplitude
+SigLim = 80;
+
+%sig_num =       size(Model,1);
+Sw =            eye(Nodes);                  % residual covariance matrix - White Noise (input Barnett Toolbox)
+AR_num =      1; %round(sig_num*AR_perc);        % number of Auto-Regressive components
+ARpos=1; 
+
+
+max_att =       Trials*100;                    
+% maximum number of attempts to generate the chosen num. of trials with the same model
+
+%%% Inputs for function "arfit_v3"
+selector=      'AIK';
+no_const=      'zero';
+
+%%% AR parameters evaluation from real data
+for r=1:AR_num
+    for tt=1:size(data,3)
+        EEG_ch = data(:,r,tt);
+        orgEEG{tt}=EEG_ch;
+    end %cycle on real trial
+    clear tt
+    ar_coef(:,r) = arfit_v3(orgEEG, p, p, selector, no_const);
+end
+
+flag=1;
+cont=0;
+
+trans_samp=zeros(1, NumIntCost-1);  
+while flag==1
+    cont=cont+1;
+    flag=0;
+    
+    %%% 1.TV model generation
+    %   1.1 Model generation in the constant intervals
+    for i=1:NumIntCost
+        startInt(i)=sum(CampInt(1:i-1))+sum(trans_samp(1:i-1))+1;
+        endInt(i)=startInt(i)-1+CampInt(i);
+        aux=reshape(repmat(Model(:,:,i),1,CampInt(i)),[Nodes Nodes CampInt(i)]);
+        TVmod(:,:,startInt(i):endInt(i))=aux;
+    end
+    clear i
+    
+    
+    %% Time-varying model 
+ 
+    for mo=1:size(TVmod,3)
+        ModelDel(:,:,:,mo)=rearrangeModel(Del_Range,TVmod(:,:,mo),DelayMatrix);
+        
+        %%% Add Auto-Regressive coefficients on the main diagonal
+        for ii=1:AR_num
+            ModelDel(ARpos(ii),ARpos(ii),:,mo)=ar_coef(1:p,ii)';
+        end
+        
+        clear ii
+    end %cycle on the number of models
+    
+    clear mo
+    
+    Tr=1;
+    num_iter=0;
+    
+    while Tr~=Trials+1
+        num_iter=num_iter+1;
+        
+        if num_iter>max_att %Max number of attempts
+            Tr=1;
+            flag=1;
+            num_iter=0;
+            break
+        end
+        
+        [Y,E]=var_to_tsdata_complete(ModelDel,Sw,DataLength,Singtr,mtrunc,decayfac);
+        Ctr=find(abs(Y)>SigLim);
+        
+        if ~isempty(Ctr)
+            continue
+        else
+            for ch=1:Nodes
+                nc=randn(1,size(Y,2));
+                Ynorm=norm(Y(ch,:));
+                ncnorm=norm(nc);
+                noise=(Ynorm/(ncnorm*sqrt(SNR)))*nc;  
+                Ynoise(ch,:)=Y(ch,:)+noise;
+            end
+            Y_gen(:,:,Tr)=Ynoise';
+            E_gen(:,:,Tr)=E';
+            Tr=Tr+1;
+        end
+    end
+    
+    clear Tr Ctr num_iter
+    flag_out=1;
+    
+    if cont>10
+        flag_out=0;
+        Y_gen=[];
+        E_gen=[];
+        break
+    end
+end
+
+%% Plot of the simulated signals and noise
+
+Nreal=size(data,1); 
+t=(0:DataLength-1)/fs; 
+t1=(0:Nreal-1)/fs; 
+figure 
+plot(t,Y_gen(:,:,1)); %sto plottando un trial 
+title('Simulated signal');
+figure
+plot(t1,data(:,:,1)); 
+title('Real signal'); 
+figure
+plot(t,E_gen(:,:,1)); 
+title('Noise'); 
+
+
+%% Proprietà spettrali 
+[p, fx]=pwelch(Y_gen(:,:,1),[],[],[],fs);
+figure
+plot(fx,p)
+title('Spectrum')
+
+%%
+[p, fx] = pwelch(Y_gen(:,:,1), [], [], [], fs);
+
+figure;
+plot(fx, 10*log(p), 'LineWidth', 2); 
+grid on;
+legend('y1','y2','y3'); 
+xlim([0 125]); 
+
+
+%% save the results 
+
+save('DelayMatrix','DelayMatrix');
+save('E_gen','E_gen'); 
+save('Model','Model');
+save('ModelDel','ModelDel');
+save('TVmod','TVmod');
+save('Y_gen','Y_gen');
